de2f78408500a2d03fcf9a2063533bd8fc35356d
Chris Hargraves <chargraves@invidi.com>
2021-12-10 16:41:18 -0500

    day 3 done

--- src/Day03.kt
+++ src/Day03.kt
@@ -2,8 +2,13 @@ fun main() {
     val input: List<String> = readInput("Day03_input")
     var gamma = ""
     var epsilon = ""
+    val oGen = input.toMutableList()
+    val co2Scrubber = input.toMutableList()
 
+
+    // part one
     for (bitPosition: Int in input.first().indices) {
+
         var oneCounter = 0
         for (item in input) {
             if (Character.getNumericValue(item[bitPosition]) == 1) {
@@ -16,8 +21,40 @@ fun main() {
 
     }
 
+    // part 2
+    for (bitPosition: Int in oGen.first().indices) {
+
+        var oneCounter = 0
+        for (item in oGen) {
+            if (Character.getNumericValue(item[bitPosition]) == 1) {
+                oneCounter += 1
+            }
+        }
+
+        oGen -= if (oneCounter >= oGen.size.toInt() - oneCounter) {
+            oGen.filter { Character.getNumericValue(it[bitPosition]) != 1 && oGen.size != 1 }.toSet()
+        } else {
+            oGen.filter { Character.getNumericValue(it[bitPosition]) != 0 && oGen.size != 1 }.toSet()
+        }
+    }
+
+    for (bitPosition: Int in co2Scrubber.first().indices) {
+
+        var oneCounter = 0
+        for (item in co2Scrubber) {
+            if (Character.getNumericValue(item[bitPosition]) == 1) {
+                oneCounter += 1
+            }
+        }
+
+        co2Scrubber -= if (oneCounter >= co2Scrubber.size.toInt() - oneCounter) {
+            co2Scrubber.filter { Character.getNumericValue(it[bitPosition]) != 0 && co2Scrubber.size != 1 }.toSet()
+        } else {
+            co2Scrubber.filter { Character.getNumericValue(it[bitPosition]) != 1 && co2Scrubber.size != 1 }.toSet()
+        }
+    }
+
     println("Power consumption: " + epsilon.toInt(2) * gamma.toInt(2))
-
-
+    println("Life Support Rating: " + oGen[0].toInt(2) * co2Scrubber[0].toInt(2))
 
 }
